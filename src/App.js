adcb960fbccafabde49b7b50aaf1bfab500c3704
Emma Wan-Kerkhove <emmawankerkhove@MacBook-Pro-de-Emma-2.local>
2021-12-15 10:23:25 +0100

    Commit Emma

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 
 import Home from './component/home';
 import Simon from './component/';
-import  from './component/';
+import Emma from './component/';
 import  from './component/';
 import  from './component/';
 import  from './component/';
