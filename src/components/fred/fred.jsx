63652e24a42ff6630391f8d59911fb5075e6b665
FFRRED <ho.frederic01@gmail.com>
2022-02-21 23:18:45 +0100

    v0.0.1 - Component info added - not finished

--- src/components/fred/fred.jsx
+++ src/components/fred/fred.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Typekit from 'react-typekit';
 import './styles.css';
 import Title from './my-components/title';
+import Info from './my-components/info';
  
 function Fred (){
  return <div className='body'>
@@ -9,6 +10,7 @@ function Fred (){
                 <Typekit kitId="uxz2jlo"/>
                 <Title/ >
                 <h1 className='lores-12 bold uppercase'>- Fredho animation -</h1>
+                <Info/ >
                 <p className='lores-12'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas, repellat? Voluptatem pariatur dolore natus ratione magni ex labore. Animi non atque ratione officia, sed, eos in reprehenderit doloribus earum enim maxime debitis iste! Fuga repellat quo minima ipsum, consectetur unde impedit a adipisci aliquid aperiam officiis optio repellendus obcaecati quibusdam veritatis illo voluptas sed facilis ut voluptatibus eos. Natus dolor hic, distinctio a voluptatem explicabo. Dolorem, corrupti obcaecati quia dolore ratione est ab debitis officia quas iure. Ut, officia? Eum dolore, amet dignissimos ipsam excepturi numquam veniam necessitatibus, rerum voluptatum beatae officia velit, id hic ad tempore odit dolorem ab sit aliquam iusto repellendus reiciendis aut. Eius sapiente illum blanditiis culpa natus numquam ut, sit, veniam autem dicta tempora mollitia expedita itaque architecto dolorem quis, voluptatem temporibus atque! Sunt accusantium, ratione labore culpa possimus aperiam voluptatem sed, vero dolorum quasi saepe. Quia ipsum id earum vitae maiores veritatis qui laudantium ut recusandae, soluta rerum alias repellendus totam blanditiis fuga facilis culpa ipsam expedita officia deleniti quibusdam dolorem consequatur! Rerum, vitae possimus, consectetur libero quam minus accusamus deleniti officia nam nulla odit at ut, illum reiciendis. Corporis consectetur maxime rem sint inventore commodi. Maxime, tempore vero? Natus nulla quam dolores labore!</p>
             </div>
          </div>
---
+++ src/components/fred/my-components/info.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Info = () => {
+    return (
+        <div className='lores-12'>
+            <div>
+                <h4 className='info-title'> Technical Info </h4>
+            </div>
+            <div>
+                <p>1920*1080</p>
+                <p>1 min 24</p>
+                <p>24 ips</p>
+            </div>
+            <div>
+                <img src="" alt="Image" />
+            </div>
+            <div>
+                <h5 className="uppercase">Process</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
